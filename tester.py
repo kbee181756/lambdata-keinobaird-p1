fb674e90d0522d71b425eee5883ba194b741aae0
Keino Baird <keinobaird@Keinos-Air.fios-router.home>
2020-05-06 11:52:00 -0400

    updated

--- tester.py
+++ tester.py
@@ -1,11 +1,12 @@
 import pandas as pd
 import numpy as np
+from null_eval import assess_NA
 
 from pandas import DataFrame
 
 df = DataFrame({'a':[1, 3, 4], 'b': [6, 4, 3]})
 print(df.head())
 
-import null_eval
+import null_eval import assess_NA
 
 print(assess_NA(df))
