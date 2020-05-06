eeb81fc119d2f058f58d1890ae4b073322d877dd
Keino Baird <keinobaird@Keinos-Air.fios-router.home>
2020-05-06 12:08:26 -0400

    added nan

--- tester.py
+++ tester.py
@@ -4,6 +4,6 @@ from null_eval.null_eval import assess_NA
 
 from pandas import DataFrame
 
-df = DataFrame({'a':[1, 3, 4], 'b': [6, 4, 3]})
+df = DataFrame({'a':[1, 3, 4], 'b': [6, np.nan, 3]})
 print(df.head())
-print(assess_NA(df))
+print(assess_NA(df))    
