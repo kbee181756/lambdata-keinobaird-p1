3a3f407d3570cccf4681c932264a3212f7668594
Keino Baird <keinobaird@Keinos-Air.fios-router.home>
2020-05-06 09:56:34 -0400

    new script

---
+++ null_eval/null_eval.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+
+def assess_NA(data):
+  null_sum = data.isnull().sum()
+  total = null_sum.sort_values(ascending=False)
+  percent = (((null_sum / len(data.index))*100).round(2) ).sort_values(ascending=False)
+  df_NA = pd.concat([total, percent], axis=1, keys=['Number of NA', 'Percent NA'])
+  df_NA = df_NA[(df_NA.T !=0).any()]
+  return df_NA
