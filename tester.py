b183a4f6b234a09c40ab98326a7cef4078a597c3
Keino Baird <keinobaird@Keinos-Air.fios-router.home>
2020-05-06 12:48:29 -0400

    3df

--- tester.py
+++ tester.py
@@ -15,3 +15,11 @@ df_2 = DataFrame({'a': [3, np.nan, 4, 6, np.nan, 5, 3],
 
 print(df_2.head())
 print(assess_NA(df_2))
+
+df_3 = DataFrame({'a': [3, np.nan, 4, 6, np.nan, np.nan, 3],
+'b':[np.nan, np.nan, 4, 6, np.nan, 5, np.nan],
+'c':[np.nan, np.nan, np.nan, np.nan, np.nan, 5, np.nan],
+'d':[np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan]})
+
+print(df_3.head())
+print(assess_NA(df_3))
