7613bc593c8e55fd5e8e7f4429d2659f3732645e
Keino Baird <keinobaird@Keinos-Air.fios-router.home>
2020-05-06 12:15:11 -0400

    second piece

--- tester.py
+++ tester.py
@@ -7,3 +7,11 @@ from pandas import DataFrame
 df = DataFrame({'a':[1, 3, 4], 'b': [6, np.nan, 3]})
 print(df.head())
 print(assess_NA(df))    
+
+df_2 = DataFrame({'a': [3, np.nan, 4, 6, np.nan, 5, 3],
+'b':[np.nan, np.nan, 4, 6, np.nan, 5, np.nan],
+'c':[np.nan, np.nan, np.nan, np.nan, np.nan, 5, np.nan],
+'d':[np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan]})
+
+print(df_2.head())
+print(assess_NA(df_2))
