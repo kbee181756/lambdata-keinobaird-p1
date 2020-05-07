26db4db2f7cfcbbd9e87e8b0e1f93056d6e2f24e
Keino Baird <keinobaird@Keinos-Air.fios-router.home>
2020-05-06 21:40:12 -0400

    new version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="null-assessor", # the name that you will install via pip
-    version="1.0",
+    version="1.1",
     author="Keino Baird",
     author_email="keinobaird@gmail.com",
     description="Assess nulls in a dataframe",
