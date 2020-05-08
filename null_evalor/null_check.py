9405c7b0a9729d9571775237b0ed421c82a50c79
Keino Baird <keinobaird@Keinos-Air.fios-router.home>
2020-05-08 10:29:24 -0400

    new tester

--- null_evalor/null_check.py
+++ null_evalor/null_check.py
@@ -2,17 +2,16 @@ import pandas as pd
 import numpy as np
 
 
-def assess_NA(data):
+def null_check(data):
     null_sum = data.isnull().sum()
-    total = null_sum.sort_values(ascending=False)
+    total = null_sum.sort_values(ascending=True)
     percent = (((null_sum / len(data.index)) * 100).round(2)
-               ).sort_values(ascending=False)
+               ).sort_values(ascending=True)
     df_NA = pd.concat([total, percent], axis=1, keys=[
         'Number of NA', 'Percent NA'])
     df_NA = df_NA[(df_NA.T != 0).any()]
     return df_NA
 
-
 if __name__ == "__main__":
-    data = assess_NA(data)
-    print(data, assess_NA(data))
+    data = null_check(data)
+    print(data, null_check(data))
--- tester2.py
+++ tester2.py
@@ -1,29 +1,28 @@
 import pandas as pd
 import numpy as np
-from null_evalor.null_eval import assess_NA
 from null_evalor.null_eval import null_check
 
 from pandas import DataFrame
 
 df = DataFrame({'a': [1, 3, 4], 'b': [6, np.nan, 3]})
-#print(df.head())
-print(assess_NA(df))
+print(df.head())
+print(null_check(df))
 
 df_2 = DataFrame({'a': [3, np.nan, 4, 6, np.nan, 5, 3],
                   'b': [np.nan, np.nan, 4, 6, np.nan, 5, np.nan],
                   'c': [np.nan, np.nan, np.nan, np.nan, np.nan, 5, np.nan],
                   'd': [np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan]})
 
-#print(df_2.head())
-print(assess_NA(df_2))
+print(df_2.head())
+print(null_check(df_2))
 
 df_3 = DataFrame({'a': [3, np.nan, 4, 6, np.nan, np.nan, 3],
                   'b': [np.nan, np.nan, 4, 6, np.nan, 5, np.nan],
                   'c': [np.nan, np.nan, np.nan, np.nan, np.nan, 5, np.nan],
                   'd': [np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan]})
 
-#print(df_3.head())
-print(assess_NA(df_3))
+print(df_3.head())
+print(null_check(df_3))
 
 print(df_3.head())
 print(null_check(df_3))
